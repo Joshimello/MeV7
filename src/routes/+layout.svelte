7119040178406c4c5d1f0051873a05742794e391
Joshimello <joshualeanjw@gmail.com>
2023-10-23 22:47:01 +0800

    .

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import '$lib/fonts.css'
+  import 'virtual:windi.css'
+</script>
+
+<div class="flex flex-col p-4 gap-16">
+  <slot/>  
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<div class="flex flex-col pt-2">
+  <span class="text-8xl leading-18 <md:text-7xl">JOSHUA LEAN</span>
+  <span class="text-3xl font-300">Feed me katsudon <br class="hidden <sm:block"/> and I will consider</span>
+</div>
+
+<div class="flex flex-col">
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">Hi I am</span>
+    <span class="text-3xl">Joshua Lean</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">Not a</span>
+    <span class="text-3xl">Frontend Developer</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">Not a</span>
+    <span class="text-3xl">Backend Admin</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">Not a</span>
+    <span class="text-3xl">UX UI Designer</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">Not a</span>
+    <span class="text-3xl">Video Editor</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">Not a</span>
+    <span class="text-3xl">3D Modeler</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">But a</span>
+    <span class="text-3xl">Student</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">At</span>
+    <span class="text-3xl">NTHU</span>
+  </div>
+  <div class="flex <md:flex-col">
+    <span class="text-3xl font-300 w-40">Studying</span>
+    <span class="text-3xl">EECS</span>
+  </div>
+</div>
+
+<div class="flex flex-col">
+  <div class="flex gap-4 text-3xl">
+    [
+    <a class="underline" href="/stuff">Stuff I did</a>
+    ]
+  </div>
+  <div class="flex gap-4 text-3xl">
+    [
+    <a class="underline" href="https://github.com/Joshimello" target="_blank">Github</a>
+    ]
+  </div>
+  <div class="flex gap-4 text-3xl">
+    [
+    <a class="underline" href="mailto:joshualeanjw@gmail.com" target="_blank">Email</a>
+    ]
+  </div>
+</div>
